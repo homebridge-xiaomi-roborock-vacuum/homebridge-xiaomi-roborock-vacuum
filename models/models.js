a18b476069a82393527aeda551f132a3a70e713c
n00d1e5 <n00d1e5@yopmail.com>
2020-03-25 11:34:55 +0100

    Add Roborock T4 to gen3 list

--- models/models.js
+++ models/models.js
@@ -15,5 +15,6 @@ module.exports = {
   'roborock.vacuum.s5e': [{speed: speedmodes.gen4, waterspeed: watermodes.gen1}],
   'roborock.vacuum.s6': [{speed: speedmodes.gen3}],
   'roborock.vacuum.t6': [{speed: speedmodes.gen3}],
+  'roborock.vacuum.t4': [{speed: speedmodes.gen3}],
   'roborock.vacuum.e2': [{speed: speedmodes.gen3}],
 };
