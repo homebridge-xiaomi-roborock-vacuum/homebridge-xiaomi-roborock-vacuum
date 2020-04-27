fed2dd71aa749beb657db58c349d97131f85349d
netRunner0 <44049295+netRunner0@users.noreply.github.com>
2020-04-27 18:52:04 +0500

    Update watermodes.js

--- models/watermodes.js
+++ models/watermodes.js
@@ -12,18 +12,18 @@ module.exports = {
     // 71-100% = "High"
     { homekitTopLevel: 100, miLevel: 203, name: "Hight" }
   ],
-gen3: [
-// 0% = Off / Aus
-{ homekitTopLevel: 0, miLevel: 0, name: "Off" },
-// 1-15% = "Soft"
-{ homekitTopLevel: 15, miLevel: 105, name: "Soft" },
-// 16-38% = "Quiet / Leise"
-{ homekitTopLevel: 38, miLevel: 101, name: "Quiet" },
-// 39-60% = "Balanced / Standard"
-{ homekitTopLevel: 60, miLevel: 102, name: "Balanced" },
-// 61-77% = "Turbo / Stark"
-{ homekitTopLevel: 77, miLevel: 103, name: "Turbo" },
-// 78-100% = "Full Speed / Max Speed / Max"
-{ homekitTopLevel: 100, miLevel: 104, name: "Max" }
-]
+  gen3: [
+    // 0% = Off / Aus
+    { homekitTopLevel: 0, miLevel: 0, name: "Off" },
+    // 1-15% = "Soft"
+    { homekitTopLevel: 15, miLevel: 105, name: "Soft" },
+    // 16-38% = "Quiet / Leise"
+    { homekitTopLevel: 38, miLevel: 101, name: "Quiet" },
+    // 39-60% = "Balanced / Standard"
+    { homekitTopLevel: 60, miLevel: 102, name: "Balanced" },
+    // 61-77% = "Turbo / Stark"
+    { homekitTopLevel: 77, miLevel: 103, name: "Turbo" },
+    // 78-100% = "Full Speed / Max Speed / Max"
+    { homekitTopLevel: 100, miLevel: 104, name: "Max" }
+  ]
 };
