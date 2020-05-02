ab8a1c6300e808711ac8e148519d081cf28b69a0
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2020-05-02 11:58:45 +0100

    Proportional steps in speed + RotationSpeed.minStep

--- index.js
+++ index.js
@@ -547,6 +547,12 @@ class XiaomiRoborockVacuum {
         }
       });
 
+      // Now that we know the model, amend the steps in the Rotation speed (for better usability)
+      const minStep = 100 / (this.findSpeedModes().length - 1);
+      this.services.fan
+        .getCharacteristic(Characteristic.RotationSpeed)
+        .setProps({ minStep });
+
       await this.getState();
       // Refresh the state every 30s so miio maintains a fresh connection (or recovers connection if lost until we fix https://github.com/nicoh88/homebridge-xiaomi-roborock-vacuum/issues/81)
       clearInterval(this.getStateInterval);
