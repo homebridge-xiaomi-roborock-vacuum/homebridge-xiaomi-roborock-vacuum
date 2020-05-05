3221ae33624a23e2df4ac9999f68a45163be5809
funkyremi <funkyremi@users.noreply.github.com>
2020-05-05 23:17:51 +0200

    Update models.js

--- models/models.js
+++ models/models.js
@@ -10,9 +10,11 @@ module.exports = {
   "roborock.vacuum.m1s": [{ speed: speedmodes.gen3 }],
   "roborock.vacuum.s5": [
     { speed: speedmodes.gen2 },
-    {firmware: ">=3.5.7", speed: speedmodes.gen3},
+    { firmware: ">=3.5.7", speed: speedmodes.gen3 },
   ],
-  "roborock.vacuum.s5e": [{ speed: speedmodes[" gen4+custom"], waterspeed: watermodes.gen1}],
+  "roborock.vacuum.s5e": [
+    { speed: speedmodes["gen4+custom"], waterspeed: watermodes.gen1 },
+  ],
   "roborock.vacuum.s6": [{ speed: speedmodes.gen4 }],
   "roborock.vacuum.t6": [{ speed: speedmodes.gen3 }],
   "roborock.vacuum.t4": [{ speed: speedmodes.gen3 }],
