d2e05e4e82ff3275bb0c0410b937639609c5540b
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2020-05-22 15:36:49 +0100

    Update S5 and default to use the gen4 speeds

--- models/models.js
+++ models/models.js
@@ -4,13 +4,13 @@ const speedmodes = require("./speedmodes");
 const watermodes = require("./watermodes");
 
 module.exports = {
-  default: { speed: speedmodes.gen1 },
+  default: { speed: speedmodes.gen4 },
   "rockrobo.vacuum.v1": [{ speed: speedmodes.gen2 }],
   "roborock.vacuum.c1": [{ speed: speedmodes.gen1 }],
   "roborock.vacuum.m1s": [{ speed: speedmodes.gen3 }],
   "roborock.vacuum.s5": [
     { speed: speedmodes.gen2 },
-    { firmware: ">=3.5.7", speed: speedmodes.gen3 },
+    { firmware: ">=3.5.7", speed: speedmodes.gen4 },
   ],
   "roborock.vacuum.s5e": [
     { speed: speedmodes["gen4+custom"], waterspeed: watermodes.gen1 },
