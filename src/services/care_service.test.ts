bdb508a14ec89e11ee1ebfb195751a6d2c182c03
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2022-08-31 01:22:31 +0200

    Care Service fully covered

--- src/services/care_service.test.ts
+++ src/services/care_service.test.ts
@@ -9,6 +9,7 @@ import {
 import { FanService } from "./fan_service";
 import { DeviceManager } from "./device_manager";
 import { HAP } from "homebridge";
+import * as HapJs from "hap-nodejs";
 
 describe("CareService", () => {
   describe("legacyCareSensors", () => {
@@ -23,6 +24,7 @@ describe("CareService", () => {
       const log = getLoggerMock();
 
       deviceManagerMock = createDeviceManagerMock();
+      deviceManagerMock.property.mockReturnValue(10);
 
       fanServices = jest.fn();
       const fan = {
@@ -49,23 +51,41 @@ describe("CareService", () => {
     test("only 1 service is exposed", () => {
       expect(careService.services).toHaveLength(1);
     });
+
+    test("the custom Care service characteristics call all getters", async () => {
+      const service = careService.services[0];
+      expect(service.characteristics).toHaveLength(5);
+      const [name, ...characteristics] = service.characteristics;
+      const onCallsHandlers = characteristics.map(
+        (characteristic) => characteristic["_events"].get
+      );
+      for (const listener of onCallsHandlers) {
+        await expect(
+          new Promise((resolve, reject) =>
+            listener((err, value) => (err ? reject(err) : resolve(value)))
+          )
+        ).resolves.toMatchSnapshot();
+      }
+    });
   });
 
   describe("nativeCareSensors", () => {
     let careService: CareService;
     let deviceManagerMock: DeviceManagerMock;
     let fanServices: jest.Mock;
-    let hap: jest.Mocked<HAP>;
+    let hap: HAP;
 
     beforeEach(() => {
       const hbMock = createHomebridgeMock();
-      hap = hbMock.hap as any;
+      hap = HapJs;
       const log = getLoggerMock();
 
       deviceManagerMock = createDeviceManagerMock();
       deviceManagerMock.property.mockReturnValue(10);
 
-      fanServices = jest.fn().mockImplementation(() => [new hap.Service.Fan()]);
+      fanServices = jest
+        .fn()
+        .mockImplementation(() => [new hbMock.hap.Service.Fan()]);
       const fan = {
         get services() {
           return fanServices();
@@ -89,6 +109,45 @@ describe("CareService", () => {
 
     test("4 services are exposed", () => {
       expect(careService.services).toHaveLength(4);
+    });
+
+    test("the fan's service characteristic calls all getters", async () => {
+      const fanService = fanServices.mock.results[0].value[0];
+      expect(fanService.getCharacteristic.mock.results).toHaveLength(2);
+      const onCallsHandlers = fanService.getCharacteristic.mock.results.map(
+        ({ value }) => value.on.mock.calls[0][1]
+      );
+      await expect(
+        new Promise((resolve, reject) =>
+          onCallsHandlers[0]((err, value) =>
+            err ? reject(err) : resolve(value)
+          )
+        )
+      ).resolves.toBe(false);
+      await expect(
+        new Promise((resolve, reject) =>
+          onCallsHandlers[1]((err, value) =>
+            err ? reject(err) : resolve(value)
+          )
+        )
+      ).resolves.toBe(99.99074074074075);
+    });
+
+    test("the FilterMaintenance services' characteristics call all getters", async () => {
+      for (const service of careService.services) {
+        expect(service.characteristics).toHaveLength(3);
+        const [name, ...characteristics] = service.characteristics;
+        const onCallsHandlers = characteristics.map(
+          (characteristic) => characteristic["_events"].get
+        );
+        for (const listener of onCallsHandlers) {
+          await expect(
+            new Promise((resolve, reject) =>
+              listener((err, value) => (err ? reject(err) : resolve(value)))
+            )
+          ).resolves.toMatchSnapshot();
+        }
+      }
     });
   });
 });
